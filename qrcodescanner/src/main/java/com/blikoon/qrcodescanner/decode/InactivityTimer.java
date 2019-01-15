39a45e57fc28a66e3fd0986a38f60967143c91b2
tobiasKaminsky <tobias@kaminsky.me>
2019-01-15 08:44:07 +0100

    change to androidx

--- qrcodescanner/src/main/java/com/blikoon/qrcodescanner/decode/InactivityTimer.java
+++ qrcodescanner/src/main/java/com/blikoon/qrcodescanner/decode/InactivityTimer.java
@@ -2,13 +2,14 @@ package com.blikoon.qrcodescanner.decode;
 
 
 import android.app.Activity;
-import android.support.annotation.NonNull;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+
+import androidx.annotation.NonNull;
 
 /**
  * Finishes an activity after a period of inactivity.
